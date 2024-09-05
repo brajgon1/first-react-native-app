2c71c858c11f28f45de99c02ade544d2dbf73270
Brandon Gonzalez <brajgon@gmail.com>
2024-09-04 23:31:24 -0600

    using flexbox and creating layouts

--- App.js
+++ App.js
@@ -3,14 +3,12 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal" />
+        <Button title="Add Goal" />
+      </View>
       <View>
-        <View>
-          <TextInput placeholder='Your Course Goal' />
-          <Button title="Add Goal" />
-        </View>
-        <View>
-          <Text>Goals:</Text>
-        </View>
+        <Text>Goals:</Text>
       </View>
     </View>
   );
@@ -19,10 +17,21 @@ export default function App() {
 // below --- makes styling reusable
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8
   }
 });
 
-// NO CSS IN REACT NATIVE --- CAN APPLY STYLES BUT DOING IT INLINE STYLES OR STYLESHEET OBJECTS ---
+// NO CSS IN REACT NATIVE --- CAN APPLY STYLES BUT DOING IT INLINE STYLES OR STYLESHEET OBJECTS --- FLEXBOX IS IMPORTANT ---
 
 // APP.JS IS THE ROOT COMPONENT IN THE APP
